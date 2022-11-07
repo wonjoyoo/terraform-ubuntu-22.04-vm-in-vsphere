3c44ca9d4a79edd7cbbc60a75cefb76064aa74a4
Sam Gabrail <sameh.gabrail@gmail.com>
2022-11-07 20:44:08 +0000

    fixed script

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    vsphere = {
+      source = "hashicorp/vsphere"
+      version = "2.2.0"
+    }
+  }
+}
+
+provider "vsphere" {
+  user                 = var.vsphere_user
+  password             = var.vsphere_password
+  vsphere_server       = var.vsphere_vcenter
+  allow_unverified_ssl = true
+}
+
 locals {
   templatevars = {
     name         = var.name,
--- output.tf
+++ output.tf
@@ -1,3 +1,3 @@
-# output "ip" {
-#   value = vsphere_virtual_machine.vm.guest_ip_addresses[0]
-# }
+output "ip" {
+  value = vsphere_virtual_machine.vm.guest_ip_addresses[0]
+}
